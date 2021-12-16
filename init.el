c3239d09cb4d40dcc4a58aa823310f6424f0b8d7
Rompicapo <spritetm55@gmail.com>
2021-12-16 20:39:10 +0100

    Added haskell syntax highlighting package

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(autothemer which-key vterm use-package rainbow-delimiters org-bullets magit ivy-rich general evil-collection doom-themes doom-modeline dashboard all-the-icons-dired)))
+   '(haskell-mode autothemer which-key vterm use-package rainbow-delimiters org-bullets magit ivy-rich general evil-collection doom-themes doom-modeline dashboard all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
